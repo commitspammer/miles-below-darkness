33e76b661804bd52e1d49067fff3f3c00f221bfe
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2024-06-14 15:33:47 -0300

    add player rotation controls

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup_cam)
         .add_systems(Startup, spawn_player)
+        .add_systems(FixedUpdate, player_movement_system)
         .run()
 }
 
@@ -20,11 +21,37 @@ fn spawn_player(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let shape = meshes.add(Capsule2d::new(25.0, 50.0)).into();
-    commands.spawn(MaterialMesh2dBundle {
+    let shape = meshes.add(Capsule2d::new(15.0, 70.0)).into();
+    commands.spawn((MaterialMesh2dBundle {
         mesh: shape,
         material: materials.add(Color::GREEN),
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
-    });
+    },
+    Player {
+        rotation_speed: 1.0,
+    }));
+}
+
+#[derive(Component)]
+struct Player {
+    rotation_speed: f32,
+}
+
+fn player_movement_system(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&Player, &mut Transform)>,
+) {
+    let (player, mut transform) = query.single_mut();
+
+    let mut rotation_factor = 0.0;
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        rotation_factor += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        rotation_factor -= 1.0;
+    }
+
+    transform.rotate_z(rotation_factor * player.rotation_speed * time.delta_seconds());
 }
