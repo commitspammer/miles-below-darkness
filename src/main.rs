51177d3ae64c515e0fa6e71138c618a1cbbcca4d
Gabriel Galvão <81876602+GabrielGRCosta@users.noreply.github.com>
2024-06-14 20:48:55 -0300

    add radar but is not working

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ fn main() {
         .add_systems(Startup, setup_cam)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_map)
+        .add_systems(Startup, spawn_line) // Adicione esta linha
         .add_systems(FixedUpdate, player_movement_system)
+        .add_systems(FixedUpdate, line_movement_system) // Adicione esta linha
         .run()
 }
 
@@ -80,4 +82,54 @@ fn player_movement_system(
     }
 
     transform.rotate_z(rotation_factor * player.rotation_speed * time.delta_seconds());
+}
+
+#[derive(Component)]
+struct Line;
+
+fn spawn_line(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let window = windows.single_mut();
+    let radius = window.resolution.height() / 2.0; // Raio do mapa
+    // A linha tem comprimento igual ao raio para que uma ponta toque o centro e a outra a borda do círculo
+    let line_mesh = Mesh::from(shape::Quad::new(Vec2::new(2.0, radius))); 
+    let line_handle = meshes.add(line_mesh);
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: line_handle.into(),
+        material: materials.add(Color::GREEN),
+        transform: Transform {
+            translation: Vec3::new(0.0, radius / 2.0, 0.0), // Posiciona a linha para que uma extremidade toque o centro
+            rotation: Quat::from_rotation_z(0.0), // Inicia apontando para cima
+            scale: Vec3::ONE,
+        },
+        ..default()
+    }).insert(Line);
+}
+
+fn line_movement_system(
+    time: Res<Time>,
+    mut query: Query<(&Line, &mut Transform)>,
+) {
+    for (_, mut transform) in query.iter_mut() {
+        let rotation_speed = 1.0; // Ajuste a velocidade de rotação conforme necessário
+        let angle = rotation_speed * time.delta_seconds();
+    
+        // Obtém a rotação atual em Euler, adiciona o ângulo, e converte de volta para Quat
+        //let current_rotation_z = transform.rotation.to_euler(EulerRot::XYZ).2;
+        let new_rotation_x = transform.rotation.to_euler(EulerRot::XYZ).2 + angle;
+        transform.rotation = Quat::from_rotation_z(new_rotation_x);
+    
+        // Calcula a nova posição usando cos e sin no ângulo de rotação
+        let radius = transform.translation.length();
+        transform.translation = Vec3::new(
+            radius * new_rotation_x.cos(), 
+            radius * new_rotation_x.sin(), 
+            0.0,
+        );
+    }
 }
