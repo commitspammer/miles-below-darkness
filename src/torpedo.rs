99ff6ad6ff7d5c2aa6f0bfd0cf9f5ab64b39a076
guilhermesilfer <guilhermeferreiradev14@gmail.com>
2024-06-21 23:16:26 -0300

    add torpedo cooldown

--- src/torpedo.rs
+++ src/torpedo.rs
@@ -2,12 +2,14 @@ use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use crate::gamestate::GameState;
 use crate::player::Player;
+use std::time::Duration;
 
 pub struct TorpedoPlugin;
 impl Plugin for TorpedoPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, spawn_torpedo_system.run_if(in_state(GameState::Game)))
-            .add_systems(Update, move_torpedo_system.run_if(in_state(GameState::Game)));
+            .add_systems(Update, move_torpedo_system.run_if(in_state(GameState::Game)))
+            .insert_resource(TorpedoCooldown(Timer::new(Duration::from_secs(1), TimerMode::Once)));
     }
 }
 
@@ -16,15 +18,22 @@ pub struct Torpedo {
     movement_speed: f32,
 }
 
+#[derive(Resource, Deref, DerefMut)]
+pub struct TorpedoCooldown(Timer);
+
 pub fn spawn_torpedo_system(
+    time: Res<Time>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut player_query: Query<(&Player, &mut Transform)>,
+    mut cooldown_timer: ResMut<TorpedoCooldown>,
 ) {
-    let (_, player_transform) = player_query.single_mut();
-    if keyboard_input.pressed(KeyCode::Space) {
+    cooldown_timer.tick(Duration::from_secs_f32(time.delta_seconds()));
+    
+    if cooldown_timer.finished() && keyboard_input.pressed(KeyCode::Space) {
+        let (_, player_transform) = player_query.single_mut();
         let shape = meshes.add(Capsule2d::new(3.0, 25.0)).into();
         commands.spawn((
             MaterialMesh2dBundle {
@@ -42,6 +51,7 @@ pub fn spawn_torpedo_system(
                 movement_speed: 100.0,
             }
         ));
+        cooldown_timer.reset();
     }
 }
 
